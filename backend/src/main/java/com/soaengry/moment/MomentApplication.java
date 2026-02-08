7a41118feed238d5eb2989e207909e19685a382e
soaeng <sodud0695@naver.com>
2026-02-09 04:43:31 +0900

    [INIT] 프로젝트 생성 및 설정

---
+++ backend/src/main/java/com/soaengry/moment/MomentApplication.java
@@ -0,0 +1,13 @@
+package com.soaengry.moment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MomentApplication.class, args);
+    }
+
+}
