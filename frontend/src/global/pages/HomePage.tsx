38a55b13466e279bdd624b7d98843f78d31a6989
soaengry <2soaeng@gmail.com>
2026-02-14 18:52:23 +0900

    [style] inline style → tailwind 변수 전환

---
+++ frontend/src/global/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="min-h-screen flex flex-col bg-bgPrimary">
+      <main className="flex-1 max-w-5xl mx-auto px-4 py-6">HOME PAGE</main>
+    </div>
+  );
+};
+
+export default HomePage;
