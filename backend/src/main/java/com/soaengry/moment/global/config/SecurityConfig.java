6b48bbf23b927cbc6ce75e4a8578ba51e53fd01c
soaengry <2soaeng@gmail.com>
2026-03-03 15:27:13 +0900

    [core] 설정 수정

--- backend/src/main/java/com/soaengry/moment/global/config/SecurityConfig.java
+++ backend/src/main/java/com/soaengry/moment/global/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         // 인증 없이 접근 가능한 경로
                         .requestMatchers(
+                                "/favicon.ico",
                                 "/hc",
                                 "/env",
                                 "/api/auth/signup",
